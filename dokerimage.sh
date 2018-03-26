34e5ace189dec973fe7d13ffb51c11328756b2c9
sarita tompe <saritatompe63@gmail.com>
2018-03-26 06:52:58 +0000

    shellscript for blockchain

---
+++ dokerimage.sh
@@ -0,0 +1,43 @@
+
+function dkrmcontainer(){
+        CONTAINER_IDS=$(docker ps -aq)
+        echo
+        if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" = " " ]; then
+                echo "========== No containers available for deletion =========="
+        else
+                docker rm -f $CONTAINER_IDS
+        fi
+        echo
+}
+function dkrmimages(){
+ 
+ 	
+	 DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
+	echo
+        if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" = " " ]; then
+		echo "========== No images available for deletion ==========="
+        else
+                docker rmi -f $DOCKER_IMAGE_IDS
+        fi
+	echo   
+
+}
+function restartNetwork() {
+	echo
+
+	  #teardown the network and clean the containers and intermediate images
+	docker-compose -f docker-compose-cli.yaml down 
+	dkrmcontainer
+	dkrmimages
+
+
+	#Start the network
+	docker-compose -f docker-compose-cli.yaml up -d 
+	
+	echo
+}
+function stopimages(){
+        restartNetwork
+}
+
+stopimages 
